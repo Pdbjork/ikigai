e04223669960c7a238abde18256de362212f80c0
pdbjork <pdbjork@gmail.com>
2025-11-30 22:03:13 -0600

    new

--- graphql_server/src/graphql/file_action/file_query.rs
+++ graphql_server/src/graphql/file_action/file_query.rs
@@ -4,7 +4,8 @@ use async_graphql::*;
 use uuid::Uuid;
 
 use crate::db::file::File;
-use crate::error::IkigaiErrorExt;
+use crate::db::{Page, PageContent};
+use crate::error::{IkigaiError, IkigaiErrorExt};
 use crate::helper::{document_quick_authorize, get_conn_from_ctx};
 
 #[derive(Default)]
@@ -27,7 +28,18 @@ impl FileQuery {
         document_quick_authorize(ctx, document_id, DocumentActionPermission::ViewDocument).await?;
 
         let mut conn = get_conn_from_ctx(ctx).await?;
-        // FIXME: Check file is existing in document
+        
+        let pages = Page::find_all_by_document_id(&mut conn, document_id).format_err()?;
+        let page_ids = pages.iter().map(|p| p.id).collect();
+        let page_contents = PageContent::find_all_by_pages(&mut conn, page_ids).format_err()?;
+        
+        let is_file_in_document = page_contents.iter().any(|content| {
+            content.get_json_content().has_file_handler(file_id)
+        });
+
+        if !is_file_in_document {
+             return Err(IkigaiError::new_bad_request("File not found in document")).format_err();
+        }
 
         let file = File::find_by_id(&mut conn, file_id).format_err()?;
         let is_mp3_file = file.content_type == "audio/mpeg";
